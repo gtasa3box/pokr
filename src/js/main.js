b3c24be9a052f2574339c9cd193cc905889a25fd
gtasa3box <gtasa3box@gmail.com>
2021-08-13 17:19:22 +0300

    adaptiv

--- src/js/main.js
+++ src/js/main.js
@@ -4,4 +4,29 @@
   * @param {number} first - первое число
   * @returns {number}
   */
+window.onload= function() {
+      document.getElementById('burgerlink').onclick = function() {
+      toogleclass(this);
+      opennav('nav', this);
+      return false;
+  };
+};
 
+function opennav(id, click_burger) {
+const div = document.getElementById(id);
+if(div.style.display == 'flex') {
+    div.style.display = 'none';
+}
+else {
+    div.style.display = 'flex';
+}
+};
+
+function toogleclass(click_burger) {
+if (click_burger.classList.contains('active')) {
+    click_burger.classList.remove('active');
+}
+else {
+    click_burger.classList.add('active');
+}
+};
